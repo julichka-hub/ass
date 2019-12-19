944763b75d2ad73759da047abc027fab2101ce11
Юлия Краснолобова <julia_morozzz@mail.ru>
2019-12-19 04:55:31 +0300

    first commit

---
+++ markin2/markin2/markin2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	double num_x;
+	cout << "Введите любое число:";
+	cin >> num_x;
+
+
+	if (num_x == 2);
+	cout << "Ответ:" << ((cos(3) * num_x + pow(num_x, 3)) / (pow(sin(num_x), 3) * num_x - 3 * num_x));
+	if (num_x > 2);
+	cout << "Ответ:" << (pow(sin(num_x), 3) + sqrt(pow(num_x, 2) + 2));
+	if (num_x < 2);
+	cout << "Ответ:" << ((pow(num_x, 3)) * (log(3 / 2)) * (abs(atan(num_x) - exp(-num_x))));
+}
